955b9426eb9b9185788cceea59b490e596cbf676
Teeratorn Moonpanyo <62991197+ping2540@users.noreply.github.com>
2020-12-03 01:50:07 +0700

    Update test.java

--- ArrayCollection/test.java
+++ ArrayCollection/test.java
@@ -36,21 +36,21 @@ public class test {
 //        System.out.println(x.toString());
 //        System.out.println(x.size());
 
-//ArrayCollection a = new ArrayCollection(1);
-//ArrayCollection c = new ArrayCollection(1);
-//a.add("A");a.add("A");a.add("C");a.add("B");
-//a.add("C"); c.add("D");c.add("A");
-//System.out.println(a.toString()); System.out.println(c.toString());
-//a.addAll(c);
-//System.out.println(a.toString()); System.out.println(c.toString());
+//        ArrayCollection a = new ArrayCollection(1);
+//        ArrayCollection c = new ArrayCollection(1);
+//        a.add("A");a.add("A");a.add("C");a.add("B");
+//        a.add("C"); c.add("D");c.add("A");
+//        System.out.println(a.toString()); System.out.println(c.toString());
+//        a.addAll(c);
+//        System.out.println(a.toString()); System.out.println(c.toString());
 
-//ArrayCollection a = new ArrayCollection(1);ArrayCollection b = new ArrayCollection(1);
-//ArrayCollection c = new ArrayCollection(1);
-//a.add("A");a.add("B");a.add("C");a.add("B");a.add("B"); a.add("B");a.add("A");
-//b.add("A"); b.add("B");b.add("C");b.add("B"); b.add("D");b.add("B");b.add("A");
-//c.add("A"); c.add("B");c.add("A");c.add("B"); c.add("B");c.add("B");c.add("A");
-//System.out.println(a.countUnique());System.out.println(b.countUnique());System.out.println(c.countUnique());
-//System.out.println(a.toString());System.out.println(b.toString());System.out.println(c.toString());
+//        ArrayCollection a = new ArrayCollection(1);ArrayCollection b = new ArrayCollection(1);
+//        ArrayCollection c = new ArrayCollection(1);
+//        a.add("A");a.add("B");a.add("C");a.add("B");a.add("B"); a.add("B");a.add("A");
+//        b.add("A"); b.add("B");b.add("C");b.add("B"); b.add("D");b.add("B");b.add("A");
+//        c.add("A"); c.add("B");c.add("A");c.add("B"); c.add("B");c.add("B");c.add("A");
+//        System.out.println(a.countUnique());System.out.println(b.countUnique());System.out.println(c.countUnique());
+//        System.out.println(a.toString());System.out.println(b.toString());System.out.println(c.toString());
  
     }
 }
